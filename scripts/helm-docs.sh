c28da0cdd8e9f28d29774c152ea4a9ba76f9faf8
Dominik Meyer <dominik@unique.ch>
2024-10-03 00:27:39 +0200

    feat: add initial scaffold and small automation

---
+++ scripts/helm-docs.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# SPDX-SnippetBegin
+# SPDX-License-Identifier: Apache License 2.0
+# SPDX-SnippetCopyrightText: 2024 © Argo Project, argoproj/argo-helm
+# SPDX-SnippetCopyrightText: 2024 © Unique AG
+# SPDX-SnippetEnd
+## Reference: https://github.com/norwoodj/helm-docs
+set -eux
+HELM_DOCS_VERSION="1.14.2"
+REPO_ROOT="$(cd "$(dirname "$0")/.." && pwd)"
+echo "Repo root: $REPO_ROOT"
+
+echo "Running helm-docs"
+docker run \
+    -v "$REPO_ROOT:/helm-docs" \
+    -u $(id -u) \
+    jnorwood/helm-docs:v$HELM_DOCS_VERSION
