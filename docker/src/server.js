52190c08e2ab10c4e6a67894e0d56cab92d5af10
Dominik from Unique <dominik@unique.ch>
2024-11-09 23:18:55 +0100

    feat: add chart-test-service (#20)

---
+++ docker/src/server.js
@@ -0,0 +1,20 @@
+const http = require('http');
+
+const hostname = '0.0.0.0';
+const port = 7777;
+
+const server = http.createServer((req, res) => {
+  if (req.url === '/probe' && req.method === 'GET') {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('OK');
+  } else {
+    res.statusCode = 404;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Not Found');
+  }
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
